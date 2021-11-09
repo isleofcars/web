3beef589a9998c72adbb0f9ada4e43da83fcf05c
killinghail606 <zhurbiym@gmail.com>
2021-11-09 16:11:46 +0200

    some updates

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -107,8 +107,7 @@ export default {
         this.isLoading = true;
         this.getMinYear();
         this.getAvailableMakes();
-
-        this.getCars(this.page, this.filtersQueryString);
+        // We get cars from: getCars() <- changeFilters() <- Filters: @changeFilters="changeFilters" <- watch.filters.emit <- mounted
     },
     methods: {
         getCars(page = 1, filters = '') {
