28464780bbccd5d5ab53c5e36d6f9802aec8ddca
sgalich <9985613@gmail.com>
2022-01-16 17:18:37 -0800

    no redundant logs

--- frontend/src/components/Base/BaseSelect.vue
+++ frontend/src/components/Base/BaseSelect.vue
@@ -169,7 +169,7 @@ export default {
             if (this.userChoseOption && this.inputValue === this.selectedOption) {
                 return this.options;
             }
-            console.log(this.options);
+            // console.log(this.options);
             // if user entered sth, we should autocomplete and suggest filtered options
             return this.options.filter((option) => option.toLowerCase().indexOf(this.inputValue.toLowerCase()) !== -1);
             // return this.options.filter((option) => option.toLowerCase().startsWith(this.inputValue.toLowerCase()));
